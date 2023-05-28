e62de68ef49bc178b7979ad21122d7652b2d2c52
Gina Castillo <gina.castillo01@uptc.edu.co>
2023-05-28 11:26:40 -0500

    graphic
    first iteration of GUI development and has not yet been tested. I am planning to make adjustments and improvements to it.

---
+++ source/IPC.py
@@ -0,0 +1,65 @@
+import threading
+import time
+import random
+import statistics
+
+class Cliente:
+    def __init__(self, id, tiempo_atencion):
+        self.id = id
+        self.tiempo_atencion = tiempo_atencion
+
+class Barberia:
+    def __init__(self, num_sillas):
+        self.sala_espera = []
+        self.num_sillas = num_sillas
+        self.is_sleep = True
+        self.mutex = threading.Semaphore(1)  # Semáforo para garantizar exclusión mutua en la sala de espera
+        self.barbero_sem = threading.Semaphore(0)  # Semáforo para despertar al barbero cuando llega un cliente
+
+    def atender_clientes(self):
+        while True:
+            self.barbero_sem.acquire()  # El barbero espera a que llegue un cliente
+            self.mutex.acquire()
+            cliente = self.sala_espera.pop(0)
+            self.mutex.release()
+
+            self.is_sleep = False
+            print(f"Barbero atendiendo al cliente {cliente.id} con un tiempo de atencion de {cliente.tiempo_atencion}")
+            time.sleep(cliente.tiempo_atencion)
+            print(f"Barbero terminó de atender al cliente {cliente.id}")
+            self.is_sleep = True
+
+    def llegada_clientes(self):
+        id_cliente = 1
+        while True:
+            tiempo_llegada = random.uniform(2, 4)
+            tiempo_atencion = random.uniform(4, 7)
+            time.sleep(tiempo_llegada)
+            cliente = Cliente(id_cliente, tiempo_atencion)
+            id_cliente += 1
+
+            self.mutex.acquire()
+            if len(self.sala_espera) < self.num_sillas:
+                self.sala_espera.append(cliente)
+                self.sala_espera.sort(key=lambda c: c.tiempo_atencion)  # Ordenar por tiempo de atención
+                print(f"Cliente {cliente.id} llegó a la sala de espera con un tiempo de llegada de {tiempo_llegada}")
+                self.mutex.release()  # Liberar el semáforo mutex antes de liberar al barbero
+                self.barbero_sem.release()  # Despertar al barbero si estaba dormido
+            else:
+                print(f"Cliente {cliente.id} se fue porque la sala de espera está llena")
+                self.mutex.release()
+            time.sleep(0.1)  # Pausa para permitir que el barbero atienda a los clientes
+
+    def simular_barbero_dormilon(self,num_sillas):
+        self.num_sillas = num_sillas  # Actualizar el número de sillas
+        threading.Thread(target=self.atender_clientes).start()
+        threading.Thread(target=self.llegada_clientes).start()
+
+
+    def get_list(self):
+        return len(self.sala_espera)
+
+    def get_sleep(self):
+        return self.is_sleep
+
+
---
+++ source/View.py
@@ -0,0 +1,200 @@
+import tkinter as tk
+from tkinter import ttk, messagebox,PhotoImage,Label, Canvas
+import threading
+from PIL import ImageTk, Image
+import os
+from IPC import Barberia
+import time
+
+class Main:
+
+    def __init__(self):
+        self.hiloSimulation = threading.Thread()
+        self.cantidad = 0
+        self.barberia = Barberia(num_sillas=self.cantidad)
+        self.create_components()
+        self.create_window()
+        self.add_components()
+        self.create_images()
+        self.barber_img('sources/images/sleep.png')
+        self.activate = False
+        #self.ventana.update()
+        self.ventana.mainloop()
+
+    def stopSimulation(self):
+        self.barberia.stop()
+        self.activate = False
+        
+
+    def startSimulation(self):
+        self.activate = True
+        self.cantidad = int(self.numeroSillas.get())
+        print(self.cantidad)
+        self.barberia.simular_barbero_dormilon(self.cantidad)
+        self.hiloBarber = threading.Thread(target=self.barber_image, args=())
+        self.hiloBarber.start()
+        self.hiloChair = threading.Thread(target=self.chairsImages, args=())
+        self.hiloChair.start()
+        
+        print(self.hiloSimulation.is_alive())
+        #self.update_gui()
+
+
+    def barber_image(self):
+        while(self.activate):
+            barber_is_sleep = self.barberia.get_sleep() 
+            if barber_is_sleep:
+                self.update_barber('sources/images/sleep.png')
+            else:
+                #print("Nooooo_________________________I_________")
+                self.update_barber('sources/images/barberWakeup.png')
+        # Actualizar el tamaño del área desplazable del canvas
+
+    def chairsImages(self):
+        while(self.activate):
+            j = 0
+            counter = 0
+            k = 0 
+            sala_espera = self.barberia.get_list()
+            if sala_espera == 0:
+                for i in range(self.cantidad):
+                    if counter == 5:
+                        j += 50
+                        k = 0
+                        counter = 0
+                    self.canvas.create_image(j, 12 + k * 50, anchor='nw', image=self.silla_imag)
+                    self.canvas.image = self.silla_imag
+                    counter += 1
+                    k += 1
+            elif sala_espera > 0:
+                if sala_espera == self.cantidad:
+                    for i in range(self.cantidad):
+                        if counter == 5:
+                            j += 50
+                            k = 0
+                            counter = 0
+                        self.canvas.create_image(j, 12 + k * 50, anchor='nw', image=self.esperando_imag)
+                        self.canvas.image = self.silla_imag
+                        counter += 1
+                        k += 1
+            if 0 < sala_espera < self.cantidad:
+                libres = self.cantidad - sala_espera
+                for i in range(self.cantidad):
+                    if counter == 5:
+                            j += 50
+                            k = 0
+                            counter = 0
+                    if i >= (self.cantidad - libres):
+                        self.canvas.create_image(j, 12 + k * 50, anchor='nw', image=self.silla_imag)
+                        self.canvas.image = self.silla_imag
+                        counter += 1
+                        k += 1
+                    else:
+                        self.canvas.create_image(j, 12 + k * 50, anchor='nw', image=self.esperando_imag)
+                        self.canvas.image = self.esperando_imag 
+                        counter += 1
+                        k += 1   
+
+    def create_components(self):
+        self.titleWindow = 'Sleepy barber'
+        self.colorFondo = '#FFFFFF'
+        self.colorFuentePrincipal = '#000000'
+        self.colorEntry='#F5F5F5'
+        self.mensajeHora='Reloj Sistema:'
+        self.mensajeTSimulacion = 'Tiempo de simulacion'
+        self.mensajeEstadoCpu='Estado CPU: '
+        self.mensajeColaProcesos = 'Cola de Procesos en:'
+        self.fuenteTitulo =('Mixed',30)
+        self.fuentePrincipal =('Mixed,20')
+        self.mensajeTablaProcesos='Cola de procesos: '
+        self.list_length_process = [3, 5, 10]
+
+
+    def create_window(self):
+        self.ventana = tk.Tk()
+        self.ventana.geometry("1000x500")
+        self.ventana.title(self.titleWindow)
+        self.ventana.resizable(0,0)
+        self.ventana.config(bg=self.colorFondo)
+
+        self.ventana.rowconfigure(0, weight=0)
+        self.ventana.rowconfigure(1, weight=1)
+        self.ventana.rowconfigure(2, weight=1)
+        self.ventana.rowconfigure(3, weight=1)
+        self.ventana.rowconfigure(4, weight=1)
+        self.ventana.rowconfigure(5, weight=1)
+        self.ventana.rowconfigure(6, weight=1)
+        self.ventana.rowconfigure(7, weight=1)
+        self.ventana.rowconfigure(8, weight=1)
+
+        self.ventana.columnconfigure(0, weight=1)
+        for i in range(1, 14):
+            self.ventana.columnconfigure(i, weight=1)
+
+    def add_components(self):
+        self.labelTitulo1=tk.Label(self.ventana,text=self.titleWindow, bg=self.colorFondo, fg=self.colorFuentePrincipal, font=self.fuenteTitulo)
+        self.labelTitulo1.grid(row=0, column=1, columnspan=5)
+
+        #Separador
+        self.separador=ttk.Separator(self.ventana,orient='horizontal')
+        self.separador.grid(row=0, column=0, sticky='SWE', columnspan=14)
+        self.separador2=ttk.Separator(self.ventana,orient='vertical')
+        self.separador2.grid(row=1, column=2, sticky='NS', rowspan=5)
+        self.separador3=ttk.Separator(self.ventana,orient='vertical')
+        self.separador3.grid(row=1, column=9, sticky='NS', rowspan=5)
+        self.separador4=ttk.Separator(self.ventana,orient='horizontal')
+        self.separador4.grid(row=5, column=0, sticky='SWE', columnspan=14)
+
+        #numero de sillas
+        self.labelEntrySimulacion=tk.Label(self.ventana,text="Numero de sillas: ",border=0, bg=self.colorFondo, fg=self.colorFuentePrincipal, font=('Mixed',15))
+        self.labelEntrySimulacion.grid(row=7, column=0, sticky='E', columnspan=2)
+        self.numeroSillas = tk.Entry(self.ventana, fg=self.colorFuentePrincipal, font=('Mixed',15))
+        self.numeroSillas.grid(row=7, column=2, sticky='E')
+        self.imageButtonSimulacion = tk.PhotoImage(file='sources/images/play.png')
+        self.imageButtonStopSimulacion = tk.PhotoImage(file='sources/images/stop.png')
+        self.bottomStarSimulacion = tk.Button(self.ventana, image=self.imageButtonSimulacion, bd=0, bg=self.colorFondo, command= self.startSimulation)
+        self.bottomStarSimulacion.grid(row=7, column=3, sticky='WE')
+        self.bottomStopSimulacion = tk.Button(self.ventana, image=self.imageButtonStopSimulacion, bd=0, bg=self.colorFondo, command= self.stopSimulation)
+        self.bottomStopSimulacion.grid(row=7, column=4, sticky='WE')
+
+    def create_images(self):
+        self.imagen_silla = Image.open('sources/images/silla.png')
+        self.imagen_silla = self.imagen_silla.resize((50, 50)) 
+        self.silla_imag = ImageTk.PhotoImage(self.imagen_silla)
+        self.imagen_esperando = Image.open('sources/images/esperandojpg.png')
+        self.imagen_esperando = self.imagen_esperando.resize((50, 50)) 
+        self.esperando_imag = ImageTk.PhotoImage(self.imagen_esperando)
+        self.canvas = Canvas(self.ventana, width=400, height=300)
+        self.canvas.grid(row=2, column=0,sticky='E', columnspan=2)
+        self.canvas.bind("<Configure>", lambda e: self.canvas.configure(scrollregion=self.canvas.bbox("all")))
+
+
+    def update_barber(self, path):
+        imagen = Image.open(path)
+        imagen = imagen.resize((200, 300)) 
+        photo = ImageTk.PhotoImage(imagen)  # Convertir a PhotoImage
+        self.sleepBarber.configure(image=photo)
+        self.sleepBarber.image = photo  # Actualizar referencia
+
+
+    
+    #Barbero
+    # Cargar la imagen
+    def barber_img(self, path):
+        imagen = Image.open(path)
+        imagen = imagen.resize((200, 300)) 
+        self.sleepBarberImage = ImageTk.PhotoImage(imagen)
+        self.sleepBarber = tk.Label(self.ventana, image=self.sleepBarberImage)
+        self.sleepBarber.config(bg='white')
+        self.sleepBarber.grid(row=2, column=2,sticky='E', columnspan=6)
+
+    def update_gui(self):
+        self.crear_imagenes()
+        self.ventana.after(500, self.update_gui)
+
+
+# Ejecutar el bucle principal de la aplicación
+main = Main()
+
+
+
