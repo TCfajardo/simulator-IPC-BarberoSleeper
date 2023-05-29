8885ab75be92192602c7205fb19dd93e27d58970
EstebanRincon12 <esteban.rincon@uptc.edu.co>
2023-05-28 20:13:40 -0500

    Se añadio la documentacion de los metodos

--- source/IPC.py
+++ source/IPC.py
@@ -5,126 +5,165 @@ import statistics
 
 
 class Cliente:
-    """
-    Represents a client in the barbershop.
-
-    Attributes:
-        id (int): The ID of the client.
-        tiempo_atencion (float): The time it takes to serve the client.
-    """
-
     def __init__(self, id, tiempo_atencion):
+        """
+        Initializes a new instance of the Cliente class.
+
+        Args:
+            id (int): The unique identifier for the client.
+            tiempo_atencion (float): The time required to attend to the client.
+        """
         self.id = id
         self.tiempo_atencion = tiempo_atencion
 
-
 class Barberia:
-    """
-    Represents a barbershop.
-
-    Attributes:
-        log (list): A log of events in the barbershop.
-        sala_espera (list): A list of clients in the waiting room.
-        num_sillas (int): The number of chairs in the waiting room.
-        is_sleep (bool): Indicates whether the barber is sleeping.
-        activate (bool): Indicates whether the simulation is active.
-        mutex (Semaphore): A semaphore to ensure mutual exclusion in the waiting room.
-        barbero_sem (Semaphore): A semaphore to wake up the barber when a client arrives.
-    """
-
     def __init__(self, num_sillas):
+        """
+        Initializes an instance of the Barberia class.
+
+        Args:
+            num_sillas (int): The number of chairs in the waiting room.
+
+        Attributes:
+            log (list): A list to store the log of events.
+            sala_espera (list): A list representing the waiting room.
+            num_sillas (int): The number of chairs in the waiting room.
+            is_sleep (bool): A flag indicating if the barber is sleeping or not.
+            activate (bool): A flag indicating if the barber shop is active or not.
+            mutex (threading.Semaphore): A semaphore for ensuring mutual exclusion in the waiting room.
+            barbero_sem (threading.Semaphore): A semaphore for waking up the barber when a customer arrives.
+            clientes_se_fueron (int): The count of customers who left without being served.
+        """
         self.log = []
-        self.sala_espera = []
+        self.waiting_room = []
         self.num_sillas = num_sillas
         self.is_sleep = True
         self.activate = False
         self.mutex = threading.Semaphore(1)  # Semáforo para garantizar exclusión mutua en la sala de espera
         self.barbero_sem = threading.Semaphore(0)  # Semáforo para despertar al barbero cuando llega un cliente
+        self.customers_gone = 0
 
     def stop(self):
         """
-        Stops the simulation.
+        Stops the operation of the barber shop.
+
+        Sets the `activate` flag to False, indicating that the barber shop should stop its operation.
         """
         self.activate = False
 
-    def atender_clientes(self):
+    def serve_customers(self):
         """
-        Barber's task to serve clients.
+        Handles the process of attending customers in the barber shop.
+
+        This method is executed by the barber thread to attend customers. It waits for a customer to arrive,
+        retrieves the customer from the waiting room, performs the service, and updates the log accordingly.
+        After attending a customer, it sets the barber's status to sleeping.
+
+        Note:
+        - This method should be executed in a separate thread.
+
         """
         while self.activate:
-            while self.activate:
-                self.barbero_sem.acquire()  # El barbero espera a que llegue un cliente
-                self.mutex.acquire()
-                cliente = self.sala_espera.pop(0)
-                self.mutex.release()
+            self.barbero_sem.acquire()  # El barbero espera a que llegue un cliente
+            self.mutex.acquire()
+            cliente = self.waiting_room.pop(0)
+            self.mutex.release()
 
-                self.is_sleep = False
-                self.log.append(
-                    "Barbero atendiendo al cliente " + str(cliente.id) + " con un tiempo de atencion de " + "{:.3}".format(
-                        cliente.tiempo_atencion) + "\n")
-                time.sleep(cliente.tiempo_atencion)
-                self.log.append("Barbero terminó de atender al cliente " + str(cliente.id) + "\n")
-                self.is_sleep = True
+            self.is_sleep = False
+            time.sleep(1)
+            self.log.append(
+                "Barbero atendiendo al cliente " + str(cliente.id) + " con un tiempo de atencion de " + "{:.3}".format(
+                    cliente.tiempo_atencion) + "\n")
+            time.sleep(cliente.tiempo_atencion)
+            self.log.append("Barbero terminó de atender al cliente " + str(cliente.id) + "\n")
+            self.is_sleep = True
 
-
-    def simular_barbero_dormilon(self, num_sillas):
+    def arrival_clients(self):
         """
-        Starts the simulation of the sleeping barber problem.
+        Simulates the arrival of customers to the barber shop.
 
-        Args:
-            num_sillas (int): The number of chairs in the waiting room.
-        """
-        self.num_sillas = num_sillas  # Update the number of chairs
-        self.activate = True #run
-        threading.Thread(target=self.atender_clientes).start()
-        threading.Thread(target=self.llegada_clientes).start()
+        This method is executed by the customer arrival thread. It generates random arrival times and service durations
+        for customers, adds them to the waiting room if there is space available, and updates the log accordingly.
 
-    def simular_barbero_dormilon(self, num_sillas):
-        self.num_sillas = num_sillas  # Actualizar el número de sillas
-        self.activate = True
-        threading.Thread(target=self.atender_clientes).start()
-        threading.Thread(target=self.llegada_clientes).start()
+        Note:
+        - This method should be executed in a separate thread.
 
-    def llegada_clientes(self):
-        """
-        Task to handle the arrival of clients.
         """
         id_cliente = 1
         while self.activate:
-            tiempo_llegada = random.uniform(1, 6)
-            tiempo_atencion = random.uniform(2, 6)
-            time.sleep(tiempo_llegada)
-            cliente = Cliente(id_cliente, tiempo_atencion)
+            arrival_time = random.uniform(2, 4)
+            attention_time = random.uniform(4, 7)
+            time.sleep(arrival_time)
+            customer = Cliente(id_cliente, attention_time)
             id_cliente += 1
 
             self.mutex.acquire()
-            if len(self.sala_espera) < self.num_sillas:
-                self.sala_espera.append(cliente)
-                self.sala_espera.sort(key=lambda c: c.tiempo_atencion)  # Ordenar por tiempo de atención
+            if len(self.waiting_room) < self.num_sillas:
+                self.waiting_room.append(customer)
+                self.waiting_room.sort(key=lambda c: c.tiempo_atencion)  # Ordenar por tiempo de atención
                 self.log.append("Cliente " + str(
-                    cliente.id) + " llegó a la sala de espera con un tiempo de llegada de " + "{:.3}".format(
-                    tiempo_llegada) + "\n")
+                    customer.id) + " llegó a la sala de espera con un tiempo de llegada de " + "{:.3}".format(
+                    arrival_time) + "\n")
                 self.mutex.release()  # Liberar el semáforo mutex antes de liberar al barbero
                 self.barbero_sem.release()  # Despertar al barbero si estaba dormido
             else:
-                self.log.append("Cliente " + str(cliente.id) + " se fue porque la sala de espera está llena \n")
+                self.customers_gone += 1
+                self.log.append("Cliente " + str(customer.id) + " se fue porque la sala de espera está llena \n")
                 self.mutex.release()
             time.sleep(0.1)  # Pausa para permitir que el barbero atienda a los clientes
 
+    def simular_barbero_dormilon(self, num_sillas):
+        """
+        Simulates the sleepy barber problem.
+
+        This method starts the simulation of the sleepy barber problem. It initializes the number of chairs in the waiting
+        room, sets the activate flag to True, and starts two threads for customer arrival and barber service.
+
+        Args:
+            num_sillas (int): The number of chairs in the waiting room.
+
+        """
+        self.num_sillas = num_sillas  # Actualizar el número de sillas
+        self.activate = True
+        threading.Thread(target=self.serve_customers).start()
+        threading.Thread(target=self.arrival_clients).start()
+
     def get_list(self):
         """
-        Returns the number of clients in the waiting room.
+        Returns the number of customers in the waiting room.
+
+        Returns:
+            int: The number of customers in the waiting room.
+
         """
-        return len(self.sala_espera)
+        return len(self.waiting_room)
+    
+    def get_clientes_se_fueron(self):
+        """
+        Returns the number of customers who left due to a full waiting room.
+
+        Returns:
+            int: The number of customers who left.
+
+        """
+        return self.customers_gone
 
     def get_sleep(self):
         """
         Returns the sleep status of the barber.
+
+        Returns:
+            bool: True if the barber is sleeping, False otherwise.
+
         """
         return self.is_sleep
 
     def get_logs(self):
         """
-        Returns the log of events in the barbershop.
+        Returns the log of events that occurred during the simulation.
+
+        Returns:
+            list: The list of log messages.
+
         """
         return self.log
--- source/View.py
+++ source/View.py
@@ -10,6 +10,10 @@ import matplotlib.pyplot as plt
 class Main:
 
     def __init__(self):
+        """
+        Initializes an instance of the class.
+
+        """
         self.hiloSimulation = threading.Thread()
         self.cantidad = 0
         self.barberia = Barberia(num_sillas=self.cantidad)
@@ -18,26 +22,32 @@ class Main:
         self.add_components()
         self.create_images()
         self.create_logs()
-        self.barber_img('sources/images/sleep.png')
+        self.barber_img('source\sources\images\sleep.png')
         self.activate = False
         # self.ventana.update()
         self.ventana.mainloop()
 
     def stopSimulation(self):
-        self.barberia.stop
-        self.activate = self.barberia.stop
+        """
+        Stops the simulation.
+
+        """
+        self.barberia.stop()
+        self.activate = False
 
     def startSimulation(self):
+        """
+        Starts the simulation.
+
+        """
         self.activate = True
         try:
-            self.cantidad = int(self.numeroSillas.get()) 
+            self.cantidad = int(self.numeroSillas.get())
             self.barberia.simular_barbero_dormilon(self.cantidad)
-
             self.hiloBarber = threading.Thread(target=self.barber_image, args=())
             self.hiloBarber.start()
             self.hiloChair = threading.Thread(target=self.chairsImages, args=())
             self.hiloChair.start()
-
             self.hiloLogs = threading.Thread(target=self.create_logs, args=())
             self.hiloLogs.start()
             print(self.hiloSimulation.is_alive())
@@ -46,6 +56,10 @@ class Main:
         # self.update_gui()
 
     def showInfo(self):
+        """
+        Displays information about the simulation.
+
+        """
         text = "---Instrucciones---\n - Ingrese el numero de asientos totales en la sala de espera en el campo " \
                "requerido\n - Pulse en el boton de play\n - Si desea pausar la simulación presione el boton de Stop\n\n" \
                "---Funcionamiento---\nUna vez iniciada la simulación, en el panel izquierdo se adicionaran las sillas " \
@@ -56,14 +70,22 @@ class Main:
         messagebox.showinfo(title="Información", message=text)
 
     def barber_image(self):
+        """
+        Updates the barber image based on the barber's sleep state.
+
+        """
         while (self.activate):
             barber_is_sleep = self.barberia.get_sleep()
             if barber_is_sleep:
-                self.update_barber('sources/images/sleep.png')
+                self.update_barber('source\sources\images\sleep.png')
             else:
-                self.update_barber('sources/images/barberWakeup.png')
+                self.update_barber('source\\sources\\images\\barberWakeup.png')
 
     def chairsImages(self):
+        """
+        Updates the images of the chairs in the waiting room based on the number of clients.
+
+        """
         while (self.activate):
             x = 0
             y = 0
@@ -104,6 +126,10 @@ class Main:
                         y += 80
 
     def create_components(self):
+        """
+        Initializes the component attributes used in the GUI.
+
+        """
         self.titleWindow = 'Sleepy Barber'
         self.colorFondo = '#FFFFFF'
         self.colorFuentePrincipal = '#000000'
@@ -118,6 +144,10 @@ class Main:
         self.list_length_process = [3, 5, 10]
 
     def create_window(self):
+        """
+        Creates the main window of the GUI with its configuration and layout.
+
+        """
         self.ventana = tk.Tk()
         self.ventana.geometry("1000x600")
         self.ventana.title(self.titleWindow)
@@ -139,33 +169,40 @@ class Main:
             self.ventana.columnconfigure(i, weight=1)
 
     def showGraphics(self):
+        """
+        Continuously updates and displays the graph showing the number of unattended clients over time.
+
+        """
         tiempos = []  # Lista para almacenar los tiempos de actualización
-        clientes_en_espera = []  # Lista para almacenar la cantidad de clientes en espera
+        clientes_no_atendidos = []  # Lista para almacenar la cantidad de clientes que no fueron atendidos
 
         while self.activate:
             tiempos.append(time.time())
-            clientes_en_espera.append(self.barberia.get_list())
+            clientes_no_atendidos.append(self.barberia.get_clientes_se_fueron())
 
             # Limitar la cantidad de puntos en la gráfica para una mejor visualización
-            if len(tiempos) > 100:
-                tiempos = tiempos[-100:]
-                clientes_en_espera = clientes_en_espera[-100:]
+            if len(tiempos) > 10000:
+                tiempos = tiempos[-10000:]
+                clientes_no_atendidos = clientes_no_atendidos[-100:]
 
             # Actualizar los datos de la línea de la gráfica
-            self.linea_grafica.set_data(tiempos, clientes_en_espera)
+            self.linea_grafica.set_data(tiempos, clientes_no_atendidos)
 
-            # Ajustar los límites del eje x para mantener los puntos anteriores visibles
+            min_y = 0 
+            max_y = max(clientes_no_atendidos) + 1  # Límite superior del eje y
             self.ax.set_xlim(min(tiempos) - 0.1, max(tiempos) + 0.1)
-            
-            # Redibujar la gráfica
-            self.fig.canvas.draw()
+            self.ax.set_ylim(min_y, max_y)
 
             # Pausa para permitir la interactividad de la ventana de la gráfica
-            plt.pause(0.1)
+            plt.pause(0.5)   
 
 
 
     def add_components(self):
+        """
+        Adds and configures the GUI components to the main window.
+
+        """
         self.labelTitulo1 = tk.Label(self.ventana, text=self.titleWindow, bg=self.colorFondo,
                                      fg=self.colorFuentePrincipal, font=self.fuenteTitulo)
         self.labelTitulo1.grid(row=0, column=1, columnspan=5)
@@ -186,8 +223,8 @@ class Main:
         self.fig, self.ax = plt.subplots(figsize=(10, 6))
         self.linea_grafica, = self.ax.plot([], [])
         self.ax.set_xlabel('Tiempo')
-        self.ax.set_ylabel('Clientes en espera')
-        self.ax.set_title('Cantidad de clientes en espera a lo largo del tiempo')
+        self.ax.set_ylabel('Clientes que se fueron')
+        self.ax.set_title('Cantidad de clientes no atendidos a lo largo del tiempo')
         self.ax.grid(True)
 
         # numero de sillas
@@ -196,24 +233,28 @@ class Main:
         self.labelEntrySimulacion.grid(row=7, column=0, sticky='E', columnspan=2)
         self.numeroSillas = tk.Entry(self.ventana, fg=self.colorFuentePrincipal, font=('Mixed', 15))
         self.numeroSillas.grid(row=7, column=2, sticky='E')
-        self.imageButtonSimulacion = tk.PhotoImage(file='sources/images/play.png')
-        self.imageButtonStopSimulacion = tk.PhotoImage(file='sources/images/stop.png')
-        self.imageButtonInfoSimulacion = tk.PhotoImage(file='sources/images/info.png')
+        self.imageButtonSimulacion = tk.PhotoImage(file='source\sources\images\play.png')
+        self.imageButtonStopSimulacion = tk.PhotoImage(file='source\sources\images\stop.png')
+        self.imageButtonInfoSimulacion = tk.PhotoImage(file='source\sources\images\info.png')
         self.bottomStarSimulacion = tk.Button(self.ventana, image=self.imageButtonSimulacion, bd=0, bg=self.colorFondo,
                                               command=self.startSimulation)
         self.bottomStarSimulacion.grid(row=7, column=3, sticky='WE')
         self.bottomStopSimulacion = tk.Button(self.ventana, image=self.imageButtonStopSimulacion, bd=0,
-                                              bg=self.colorFondo, command=self.stopSimulation())
+                                              bg=self.colorFondo, command=self.stopSimulation)
         self.bottomStopSimulacion.grid(row=7, column=4, sticky='WE')
         self.bottomInfoSimulacion = tk.Button(self.ventana, image=self.imageButtonInfoSimulacion, bd=0,
                                               bg=self.colorFondo, command=self.showInfo)
         self.bottomInfoSimulacion.grid(row=7, column=5, sticky='WE')
 
     def create_images(self):
-        self.imagen_silla = Image.open('sources/images/silla.png')
+        """
+        Loads and creates image objects for the GUI.
+
+        """
+        self.imagen_silla = Image.open('source\sources\images\silla.png')
         self.imagen_silla = self.imagen_silla.resize((80, 80))
         self.silla_imag = ImageTk.PhotoImage(self.imagen_silla)
-        self.imagen_esperando = Image.open('sources/images/esperandojpg.png')
+        self.imagen_esperando = Image.open('source\sources\images\esperandojpg.png')
         self.imagen_esperando = self.imagen_esperando.resize((80, 80))
         self.esperando_imag = ImageTk.PhotoImage(self.imagen_esperando)
         self.canvas = Canvas(self.ventana, width=540, height=180)
@@ -222,6 +263,10 @@ class Main:
         self.canvas.bind("<Configure>", lambda e: self.canvas.configure(scrollregion=self.canvas.bbox("all")))
 
     def create_logs(self):
+        """
+        Creates a text area to display logs.
+
+        """
         scroll = tk.Scrollbar(self.ventana, orient='vertical')
 
         self.logs = tk.Text(self.ventana, width=60, height=8, background="white", foreground="Black",
@@ -243,9 +288,16 @@ class Main:
                 aux_text = str(self.barberia.get_logs())
                 time.sleep(0.7)
         except AttributeError:
-            print("Error")
+            print()
 
     def update_barber(self, path):
+        """
+        Updates the image of the barber in the GUI.
+
+        Args:
+            path (str): The file path of the new image.
+
+        """
         imagen = Image.open(path)
         imagen = imagen.resize((200, 300))
         photo = ImageTk.PhotoImage(imagen)  # Convertir a PhotoImage
@@ -255,6 +307,13 @@ class Main:
     # Barbero
     # Cargar la imagen
     def barber_img(self, path):
+        """
+        Creates and displays the initial image of the barber in the GUI.
+
+        Args:
+            path (str): The file path of the image.
+
+        """
         imagen = Image.open(path)
         imagen = imagen.resize((200, 300))
         self.sleepBarberImage = ImageTk.PhotoImage(imagen)
@@ -263,6 +322,9 @@ class Main:
         self.sleepBarber.grid(row=2, column=2, sticky='E', columnspan=6)
 
     def update_gui(self):
+        """
+        Updates the GUI components periodically.
+        """
         self.crear_imagenes()
         self.ventana.after(500, self.update_gui)
 
